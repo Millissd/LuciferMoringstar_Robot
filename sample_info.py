362236eae063354f01a27e607844a3d151abe512
Millissd <96375465+Millissd@users.noreply.github.com>
2021-12-20 14:21:25 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,24 +1,24 @@
 # Bot information
-SESSION = 'LuciferMoringstar_Robot'
+SESSION = 'Lucifer_Morning_Star_BoT'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+API_ID = 1061393
+API_HASH = 'dee44b60afe8fd325a96f5237e495d7a'
+BOT_TOKEN = '2003090343:AAFe8YK26KuBP-lXIxXII9cvjk4anOWT1Qc'
+USERBOT_STRING_SESSION = 'Lucifer_Morning_Star_BoT'
 
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
+ADMINS = [783435461, 'papaplease', 1211777031]
+CHANNELS = [-1001207104404, -1001572960661,]
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_URI = "mongodb+srv://Venuygop:Venuygop@cluster0.rmrsh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME = 'Venugs'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
